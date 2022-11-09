63c69fc565a7bee75e0338975c86ac001f189018
Ganesh Venkatachalam <ganeshece1317@gmail.com>
2022-11-09 14:53:53 -0500

    Adding task9 details

--- task9/multi_cache_stress.c
+++ task9/multi_cache_stress.c
@@ -1,29 +1,59 @@
 // CPP Program to find sum of array
 #include <stdio.h>
+#include<stdlib.h>
 #include <pthread.h>
-
+//20% cache miss
 // size of array
-#define MAX 1024
+#define MAX 2048
 
 // maximum number of threads
 #define MAX_THREAD 4
 
 
-int a[MAX] = {0};
-int sum[MAX] = { 0 };
-int part = 0;
+volatile double var1, var2;
+volatile double a[MAX] = {0};
 
-void* sum_array(void* arg)
+
+// void* thread_fn(void* arg)
+// {
+// 	a[0] = a[512]*2;
+//     a[1024] = a[1549]+5;
+// }
+
+void* thread_fn(void* arg)
 {
+    int *tid = (int *)arg;
+    
+    for(int i = *tid; i<MAX;i=i+MAX_THREAD){
+        a[i] += 10;
+    }
+	// if(*tid == 0){
+    //     for(int i = 0; i<MAX; i++){
+    //         var1 += a[i]*2.0;
+    //         var2 += a[i]/8;
+    //     }
+    // }
+    // if(*tid == 1){
+    //     for(int i = 0; i<MAX; i++){
+    //         var2 *= a[i]*2.0;
+    //         var1 *= a[i]+4;
+    //     }
+    // }
+    // if(*tid == 2){
+    //     for(int i = 0; i<MAX; i++){
+    //         var2 *= a[i]-2.0;
+    //         var1 *= a[i]*5;
+    //         a[i] += var1 + var2;
+    //     }
+    // }
+    // if(*tid == 3){
+    //     for(int i = 0; i<MAX; i++){
+    //         var2 *= a[i]--;
+    //         var1 *= a[i]/4;
+    //     }
+    // }
 
 
-	// Each thread computes sum of 1/4th of array
-	int thread_part = part++;
-    int i;
-	//for (i = thread_part * (MAX / 4); i < (thread_part + 1) * (MAX / 4); i++)
-	//	sum[thread_part] += a[i];
-    for (i = 0; i < MAX; i++)
-		sum[i] += a[i];
 }
 
 // Driver Code
@@ -31,18 +61,21 @@ int main()
 {
 
 	pthread_t threads[MAX_THREAD];
+    for(int i = 0; i<MAX;i++){
+        a[i] = (double)rand()/1.0;
+    }
     while(1){
         // Creating 4 threads
         int i;
         for (i = 0; i < MAX_THREAD; i++)
-            pthread_create(&threads[i], NULL, sum_array, (void*)NULL);
+            pthread_create(&threads[i], NULL, thread_fn, (void*)&i);
 
         // joining 4 threads i.e. waiting for all 4 threads to complete
-        for (i = 0; i < MAX_THREAD; i++)
-            pthread_join(threads[i], NULL);
+        //for (i = 0; i < MAX_THREAD; i++)
+        //    pthread_join(threads[i], NULL);
 
         // adding sum of all 4 parts
-        int total_sum = 0;
+        //int total_sum = 0;
         // for (i = 0; i < MAX_THREAD; i++){
         //     printf("sum is %d\n", sum);
         // }
