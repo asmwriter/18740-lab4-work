b96a91c143f1eb88b0a5b28023a83d1236361ffb
Aakriti Saraf <amsaraf@andrew.cmu.edu>
2022-11-13 17:06:24 -0500

    Getting 6+

--- task7/bp_stress.c
+++ task7/bp_stress.c
@@ -1,30 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<stdint.h>
 
-#include <stdint.h>
-
-unsigned long long int rand1() {
-    volatile unsigned long long int r;
-    while(((r = rand() % 1024) < 512) );
-    return r;
-}
-
-unsigned long long int  rand2() {
-    volatile unsigned long long int r;
-    while(((r = rand() % 1024) >= 512) );
-    return r;
-}
-
-#define UNROLL_TEST \
-        rand_no = rand1(); \
-        if(rand_no < 256){      \
-            taken++;            \
-        }         \
-		rand_no = rand2();              \
-        if(rand_no >= 256){     \
-            not_taken++;         \
-        }                       \
+#define UNROLL_TEST                 \
+        rand_no = random()%1024;    \
+        if(rand_no < 512){          \
+            not_taken++;            \
+        }                           \
+        if(rand_no >= 512){         \
+            taken++;                \
+        }                           \
 
 #define UNROLL_TEST_10 UNROLL_TEST UNROLL_TEST UNROLL_TEST UNROLL_TEST UNROLL_TEST UNROLL_TEST UNROLL_TEST UNROLL_TEST UNROLL_TEST UNROLL_TEST
 #define UNROLL_TEST_100 UNROLL_TEST_10 UNROLL_TEST_10 UNROLL_TEST_10 UNROLL_TEST_10 UNROLL_TEST_10 UNROLL_TEST_10 UNROLL_TEST_10 UNROLL_TEST_10 UNROLL_TEST_10
@@ -32,12 +18,14 @@ unsigned long long int  rand2() {
 #define UNROLL_TEST_10000 UNROLL_TEST_1000 UNROLL_TEST_1000 UNROLL_TEST_1000 UNROLL_TEST_1000 UNROLL_TEST_1000 UNROLL_TEST_1000 UNROLL_TEST_1000 UNROLL_TEST_1000 UNROLL_TEST_1000 UNROLL_TEST_1000
 
 int main(){
-    unsigned long long int rand_no;
-	 
+    int rand_no; 
     int taken = 0, not_taken = 0;
     while(1){
         UNROLL_TEST_10000
-		
+		UNROLL_TEST_10000
+		UNROLL_TEST_10000
+		UNROLL_TEST_10000
+		UNROLL_TEST_10000
     }
     return 0;
 
